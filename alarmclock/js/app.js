aef77fb375ef963c1dccd287863ecf5b1ff5d34c
sebastienbw <sebastienbw@gmail.com>
2015-02-25 16:38:44 -0800

    large rotater

--- alarmclock/js/app.js
+++ alarmclock/js/app.js
@@ -1,17 +1,29 @@
-// SAMPLE CODE
-var myElement = document.getElementById('myElement');
+// SAMPLE CODE//////////////////////////////////
+// var myElement = document.getElementById('myElement');
 
-// create a simple instance
-// by default, it only adds horizontal recognizers
-var mc = new Hammer(myElement);
+// // create a simple instance
+// // by default, it only adds horizontal recognizers
+// var mc = new Hammer(myElement);
 
-// listen to events...
-mc.on("panleft panright tap press", function(ev) {
-    myElement.textContent = ev.type +" gesture detected.";
-});
-// END SAMPLE CODE
+// // listen to events...
+// mc.on("panleft panright tap press", function(ev) {
+//     myElement.textContent = ev.type +" gesture detected.";
+// });
+// END SAMPLE CODE//////////////////////////////
 
-//SKYDISC
+//SKYDISC///////////////////////////////////////
+var skydiscWidth = $('#skydisc').width();
+var skydiscHeight = $('#skydisc').height();
+if (skydiscWidth > skydiscHeight) {
+	$('#skydisc').css({
+	    'height': skydiscWidth + 'px'
+	});
+} else {
+	$('#skydisc').css({
+	    'width': skydiscHeight + 'px'
+	});
+}
+
 var skydiscRotation = 0;
 var skydisc = document.getElementById("skydisc");
 var sd = new Hammer(skydisc);
@@ -26,4 +38,5 @@ sd.on("panright", function(ev) {
 	skydiscRotation++;
 	console.log(skydiscRotation);
 	skydisc.style.transform ="rotate("+skydiscRotation+"deg)";
-})
+});
+// END SKYDISC/////////////////////////////////
